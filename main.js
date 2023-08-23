dda473d1e8aa9b2addeb5ffa6f37669fa7c3a9ca
Nathan Mannes <nmannes@gmail.com>
2023-08-23 18:55:06 -0500

    loop through all the games

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hello, world!')
+const { SlippiGame } = require("@slippi/slippi-js");
+const fs = require('fs');
+
+const path = '/home/nathan/Desktop/smash-sight/replays';
+var files = [];
+fs.readdirSync(path).forEach(month => {
+    const folderPath = `${path}/${month}`; 
+    fs.readdirSync(folderPath).forEach(file => {
+        files.push(`${folderPath}/${file}`);
+    })
+});
+
+
+// Get metadata - start time, platform played on, etc
+files.forEach( file => {
+    const game = new SlippiGame(file);
+    const settings = game.getSettings();
+    const metadata = game.getMetadata();
+})
+
+console.log('done!')

79dece1c5bcfd8aa99f91bce0ca4c8ae0eb9a289
Nathan Mannes <nmannes@gmail.com>
2023-08-25 18:44:03 -0500

    determine opening type count for each character

--- main.js
+++ main.js
@@ -36,15 +36,18 @@ for (var i = 0; i < files.length; i++) {
 
 const playerCode = Object.keys(sums).reduce((a, b) => sums[a] > sums[b] ? a : b);
 
-var stats = {}
+var counts = {}
 
 for (var i = 0; i < files.length; i++) {
+    if (i % 15 === 0){
+        console.log(i / files.length)
+    }
     const game = new SlippiGame(files[i]);
     const metadata = game.getMetadata();
     if (!metadata || !metadata.players || Object.keys(metadata.players).length !== 2) {
         continue;
     }
-    
+
     var playerIndex = '0'
     var opponentIndex = '1'
     if (metadata.players['0'].names.code !== playerCode) {
@@ -53,21 +56,57 @@ for (var i = 0; i < files.length; i++) {
     }
 
     var oppoChar = metadata.players[opponentIndex].characters;
-
     var stats = game.getStats();
-    const keys = Object.keys(stats);
-    console.log(keys);
 
-    for (var j = 0; j < stats.conversions.length; j++){
-        console.log(Object.keys(stats.conversions[j]));
-        if (stats.conversions[j].playerIndex === Number(opponentIndex)){
+    for (var j = 0; j < stats.conversions.length; j++) {
+        if (stats.conversions[j].playerIndex === Number(opponentIndex)) {
             // todo: figure out ratio of neutral-win/counterattack
             // for each character
-            stats.conversions
+            counts[oppoChar] ||= {};
+            counts[oppoChar]['neutral-win'] ||= 0;
+            counts[oppoChar]['counter-attack'] ||= 0;
+            counts[oppoChar][stats.conversions[j].openingType] += 1;
         }
     }
-
-    break;
 }
+console.log(counts)
 
 
+
+/*
+export declare enum Character {
+    CAPTAIN_FALCON = 0,
+    DONKEY_KONG = 1,
+    FOX = 2,
+    GAME_AND_WATCH = 3,
+    KIRBY = 4,
+    BOWSER = 5,
+    LINK = 6,
+    LUIGI = 7,
+    MARIO = 8,
+    MARTH = 9,
+    MEWTWO = 10,
+    NESS = 11,
+    PEACH = 12,
+    PIKACHU = 13,
+    ICE_CLIMBERS = 14,
+    JIGGLYPUFF = 15,
+    SAMUS = 16,
+    YOSHI = 17,
+    ZELDA = 18,
+    SHEIK = 19,
+    FALCO = 20,
+    YOUNG_LINK = 21,
+    DR_MARIO = 22,
+    ROY = 23,
+    PICHU = 24,
+    GANONDORF = 25,
+    MASTER_HAND = 26,
+    WIREFRAME_MALE = 27,
+    WIREFRAME_FEMALE = 28,
+    GIGA_BOWSER = 29,
+    CRAZY_HAND = 30,
+    SANDBAG = 31,
+    POPO = 32
+}
+*/

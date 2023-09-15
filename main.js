383d69a8d2ea4e3918b7a40bf01fb8326cafa6b6
Nathan Mannes <nmannes@gmail.com>
2023-09-15 13:45:26 -0500

    move functions into a utils file

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const { SlippiGame } = require("@slippi/slippi-js");
 const slp = require("@slippi/slippi-js");
 const crypto = require('crypto');
 const fs = require('fs');
-const { exec } = require('node:child_process')
+const { exec } = require('node:child_process');
+const utils = require("./utils");
 
 
 exec(`rm -rf /tmp/tmp-*`);
@@ -17,41 +17,12 @@ fs.readdirSync(path).forEach(month => {
 });
 
 // determines player name to analyze
-
-var sums = {}
-for (var i = 0; i < files.length; i++) {
-    const game = new SlippiGame(files[i]);
-    const metadata = game.getMetadata();
-    if (metadata && metadata.players) {
-        for (const key in metadata.players) {
-            const playerCode = metadata.players[key].names.code;
-            if (playerCode in sums) {
-                sums[playerCode] += 1;
-            } else {
-                sums[playerCode] = 1;
-            }
-        }
-    }
-}
-
-const playerCode = Object.keys(sums).reduce((a, b) => sums[a] > sums[b] ? a : b);
+const playerCode = utils.getPlayerCode(files);
 
 var queue = [];
 
 for (var i = 0; i < 1; i++) {
-    const game = new SlippiGame(files[i]);
-    const metadata = game.getMetadata();
-    if (!metadata || !metadata.players || Object.keys(metadata.players).length !== 2) {
-        continue;
-    }
-
-    var playerIndex = '0'
-    var opponentIndex = '1'
-    if (metadata.players['0'].names.code !== playerCode) {
-        playerIndex = '1'
-        opponentIndex = '0'
-    }
-
+    const [game, _, opponentIndex] = utils.indexGame(files[i], playerCode);
     var stats = game.getStats();
     for (var j = 0; j < stats.conversions.length; j++) {
         if (stats.conversions[j].playerIndex === Number(opponentIndex)) {
@@ -77,7 +48,7 @@ const jsonFilePath = `/home/nathan/Desktop/smash-sight/generated_json/${id}.json
 fs.writeFileSync(jsonFilePath, JSON.stringify(jsonInput), function (err) {
     if (err) throw err;
     console.log('Saved!');
-}); 
+});
 
 
 exec(`cd ../slp-to-video; node slp_to_video ${jsonFilePath}`)

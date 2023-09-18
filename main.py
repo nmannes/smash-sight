401b37667ff55a27b32ca3a4a727b50760d4b2ea
Nathan Mannes <nmannes@gmail.com>
2023-09-17 22:57:01 -0500

    fixing errors from earlier

--- main.py
+++ main.py
@@ -24,21 +24,23 @@ for character in data.keys():
         else:
             clusters[cluster] = [data[character]['labels'][i]]
     print(character, len(clusters.keys()))
-    queue = []
     for key in clusters:
+        totalFrames = 0
+        queue = []
         for conv in clusters[key]:
+            if totalFrames > 60 * 60:
+                break
             queue.append({
                 "path": conv[4],
                 "startFrame": conv[2],
                 "endFrame": conv[3],
             })
-    output.append({
-        "outputPath": "./generated_videos/{}/{}-{}.mp4".format(run_id, character, key),
-        "queue": queue, 
-    })
+            totalFrames += conv[3] - conv[2]
+        output.append({
+            "outputPath": "./generated_videos/{}/{}-{}.mp4".format(run_id, character, key),
+            "queue": queue, 
+        })
 
 f = open('./generated_json/{}.json'.format(run_id), 'w')
 f.write(json.dumps(output))
 f.close()
-
-print ('done python')
